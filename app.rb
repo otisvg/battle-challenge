e34ad6e122477fd201801c4c66fc81d40d66169e
Otis <otisvickersgraver@gmail.com>
2020-10-13 14:42:10 +0100

    created views and imported html

--- app.rb
+++ app.rb
@@ -17,9 +17,6 @@ get '/love' do
   "<3"
 end
 
-get '/cat' do
-  "</div style=border:'5px dashed red'>
-  <img src=http://f.cl.ly/items/0k0v3e2X3l2f3i1n1Y19/Screen%20Shot%202013-09-10%20at%2011.32.00.png>
-  </div>
-  "
+get '/cats' do
+  erb(:index)
 end
