7d6437c265650ebea1ecdf078e1f41e1928a7ed8
Otis <otisvickersgraver@gmail.com>
2020-10-13 16:39:21 +0100

    removed useless routes

--- app.rb
+++ app.rb
@@ -1,22 +1,8 @@
 require 'sinatra'
 require 'shotgun'
 
-
-
 get '/' do
   "home"
-end
-
-get '/secret' do
-  "this is a secret page! you must be very SPECIAL :O"
-end
-
-get '/otis' do
-  "i'm otis :D"
-end
-
-get '/love' do
-  "<3"
 end
 
 get '/random-cats' do
